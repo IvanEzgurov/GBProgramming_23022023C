58d7c3545ed4351ba4e4fd10b8c722fbc29f3a9c
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-02 17:39:42 +0300

    Projek4

--- Projekt4/Program.cs
+++ Projekt4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введи зх значное число ");
+int num = int.Parse(Console.ReadLine()); 
+int result = num % 10;
+
+Console.Write($"Последняя цифрв числа =  ");
+Console.Write(result);
