2ef15e99199162538398ed929834abd0815370bd
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-03-02 17:22:18 +0300

    Projek3.1

--- Projekt3/Program.cs
+++ Projekt3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число дня недели: ");
+int day = int.Parse(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+    Console.Write("Понедельник");
+    break;
+    case 2:
+    Console.Write("Вторник");
+    break;
+    case 3:
+    Console.Write("Среда");
+    break;
+    case 4:
+    Console.Write("Четверг");
+    break;
+    case 5:
+    Console.Write("Пятница");
+    break;
+    case 6:
+    Console.Write("Суббота");
+    break;
+    case 7:
+    Console.Write("Воскресенье");
+    break;
+    default:
+    Console.Write("Нет такого дня недели");
+    break;
+
+
+}
